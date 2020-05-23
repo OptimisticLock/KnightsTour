da90dd76f57d180a4c5066d8fc2600c49cd58716
iii <izgitiii@optimisticlock.com>
2020-05-23 15:22:16 -0700

    printing matrix

--- KnightsTour/Program.cs
+++ KnightsTour/Program.cs
@@ -2,11 +2,26 @@
 
 namespace KnightsTour
 {
+
+    // TODO are tests required? 
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+            const int width = 8;
+            const int height = 8;
+            int[,] matrix = new int[8, 8];
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                    Console.Write(matrix[x, y]);
+                Console.WriteLine();
+            }
+            Console.WriteLine("Done");
+
         }
     }
 }
