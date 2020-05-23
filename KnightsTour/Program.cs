655a09b00d19af52d08436e0d3416c9b74f3e478
iii <izgitiii@optimisticlock.com>
2020-05-23 15:54:12 -0700

    string interpolation

--- KnightsTour/Program.cs
+++ KnightsTour/Program.cs
@@ -8,16 +8,17 @@ namespace KnightsTour
     {
         static void Main(string[] args)
         {
-
-
             const int width = 8;
             const int height = 8;
             int[,] matrix = new int[8, 8];
+            matrix[1, 1] = 1;
+            matrix[2, 2] = 2;
+            matrix[3, 3] = 33;
 
             for (int y = 0; y < height; y++)
             {
                 for (int x = 0; x < width; x++)
-                    Console.Write(matrix[x, y]);
+                    Console.Write($"{matrix[x, y], 3}");
                 Console.WriteLine();
             }
             Console.WriteLine("Done");
