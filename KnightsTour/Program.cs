50bde5ceaed88fe94ef39c97eaa60e2b47e0461f
iii <izgitiii@optimisticlock.com>
2020-05-23 17:26:08 -0700

    ugly refactoring, don't look

--- KnightsTour/Program.cs
+++ KnightsTour/Program.cs
@@ -9,25 +9,38 @@ using System;
 
 namespace KnightsTour
 {
+    class Matrix
+    {
+        public const int width = 8;
+        public const int height = 8;
+        public int[,] matrix = new int[8, 8];
+
+        public static Matrix sample()
+        {
+            Matrix matrix = new Matrix();
+            matrix.matrix[1, 1] = 1;
+            matrix.matrix[2, 2] = 2;
+            matrix.matrix[3, 3] = 33;
+            return matrix;
+        }
+
+        
+    }
 
     // TODO are tests required? 
     class Program
     {
         static void Main(string[] args)
         {
-            const int width = 8;
-            const int height = 8;
 
             Console.WriteLine("Start");
 
-            int[,] matrix = new int[8, 8];
-            matrix[1, 1] = 1;
-            matrix[2, 2] = 2;
-            matrix[3, 3] = 33;
+            int[,] matrix = Matrix.sample().matrix;
 
-            for (int y = 0; y < height; y++)
+
+            for (int y = 0; y < Matrix.height; y++)
             {
-                for (int x = 0; x < width; x++)
+                for (int x = 0; x < Matrix.width; x++)
                     Console.Write(matrix[x, y] == 0 ? "  ." : $"{matrix[x, y], 3}");           
                 Console.WriteLine();
             }
